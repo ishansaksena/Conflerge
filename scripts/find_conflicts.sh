ec65ba6d9fbe9f01e388f5e0d5d5695495f54217
John Laws Harrison <johnlawsharrison@gmail.com>
2017-02-22 11:37:26 -0800

    Use context flags for git commands

--- scripts/find_conflicts.sh
+++ scripts/find_conflicts.sh
@@ -7,32 +7,32 @@ touch merge_conflicts.txt
 while read MERGE
 do
 	# Get the merge's parent commits
-	COMMIT_1=$(git rev-parse $MERGE^1)
-	COMMIT_2=$(git rev-parse $MERGE^2)
-	
-	git checkout --force -b commit1 $COMMIT_1	
-	git checkout --force -b commit2 $COMMIT_2
+	COMMIT_1=$(git -C $1 rev-parse $MERGE^1)
+	COMMIT_2=$(git -C $1 rev-parse $MERGE^2)
+
+	git -C $1 checkout --force -b commit1 $COMMIT_1
+	git -C $1 checkout --force -b commit2 $COMMIT_2
 
 	# Attempt to merge the parents
-	git checkout commit1
-	git merge commit2 > merge.txt
+	git -C $1 checkout commit1
+	git -C $1 merge commit2 > merge.txt
 
 	# Handle conflicts
 	CONFLICTS=$(grep CONFLICT merge.txt | grep content | wc -l)
 	if [ $CONFLICTS -gt 0 ]; then
 			echo "$COMMIT_1 $COMMIT_2 $MERGE" >> merge_conflicts.txt
-			git reset --merge
+			git -C $1 reset --merge
 			echo "--------------------------------"
 			echo "   Found $CONFLICTS conflicts" 
 			echo "--------------------------------"
 	fi
 
 	# Clean up
-	git checkout master
-	git branch -D commit1
-	git branch -D commit2
-	git reset --hard master	
-done <<< "$(git rev-list --merges --max-parents=2 HEAD)"
+	git -C $1 checkout master
+	git -C $1 branch -D commit1
+	git -C $1 branch -D commit2
+	git -C $1 reset --hard master
+done <<< "$(git -C $1 rev-list --merges --max-parents=2 HEAD)"
 
 rm merge.txt
 
