bb22c59fa8af4471e075097b1cce4c918dab7188
John Laws Harrison <johnlawsharrison@gmail.com>
2017-02-25 22:51:15 -0800

    Add python script for iterating through repos

---
+++ scripts/test_repos.py
@@ -0,0 +1,16 @@
+# script to run Conflerge testing a list of GitHub repos one by one
+# TODO: parallelize this
+import subprocess
+
+from github import Github
+
+g = Github("user", "pass")
+
+for repo in g.search_repositories("language:java", sort="stars", order="desc"):
+	# clone the repo to /tmp/
+	p = subprocess.Popen("git clone {0} && mv {1} /tmp/".format(repo.ssh_url, repo.name), shell=True).wait()
+
+	# test the repo using Conflerge
+	p = subprocess.Popen("./test_repo.sh {0} {1}".format(repo.name, repo.full_name.replace('/', '-')), shell=True).wait()
+	# cleanup
+	p = subprocess.Popen("rm -rf /tmp/{0}".format(repo.name), shell=True).wait()
