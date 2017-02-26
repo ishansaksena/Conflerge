44a20152828507ce59ae8060746c543808e43a57
John Laws Harrison <johnlawsharrison@gmail.com>
2017-02-25 19:42:30 -0800

    Output testing files into separate directories

--- scripts/test_repo.sh
+++ scripts/test_repo.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-REPO_FULL_NAME=$1
+REPO_FULL_NAME=$2
 REPO_DIR=/tmp/$(cut -d'-' -f2 <<< $1)
-RESULTS_DIR=$REPO_FULL_NAME_test_results
+RESULTS_DIR=${REPO_FULL_NAME}_test_results
 
-# create a dir to store all of our test results in for this repo
-if [ ! -d $RESULTS_DIR ]
-then
-	mkdir $RESULTS_DIR
+
+# create a directory to store this repo's data
+if [ ! -d ${RESULTS_DIR} ]
+	then
+	mkdir ${RESULTS_DIR}
 fi
 
 # Find all merge conflicts for the given repo
-if [ ! -f $RESULTS_DIR/merge_conflicts.txt ]
-then
-  ./find_conflicts.sh $REPO_DIR $RESULTS_DIR
+if [ ! -f {$RESULTS_DIR}/merge_conflicts.txt ]
+	then
+	# copy if this is one of the repos already checked (in previous versions of this script)
+	if [ -f /tmp/${REPO_FULL_NAME}_conflicts.txt ]
+		then
+		cp /tmp/${REPO_FULL_NAME}-conflicts.txt ${RESULTS_DIR}/merge_conflicts.txt
+	else
+		./find_conflicts.sh $REPO_DIR $RESULTS_DIR
+	fi
 fi
 
 # Run Conflerge on found conflicts in the repo
