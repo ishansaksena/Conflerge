38dc37e18a45284264b6f19bc4243d164c4b7d77
GlennHanawalt <grh95@cs.washington.edu>
2017-02-25 17:14:31 -0800

    added support for import statements

--- src/main/java/conflerge/merger/TreeMerger.java
+++ src/main/java/conflerge/merger/TreeMerger.java
@@ -2,10 +2,16 @@ package conflerge.merger;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.ImportDeclaration;
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.utils.Pair;
 
 import conflerge.differ.ast.ASTDiffer;
@@ -21,19 +27,21 @@ public class TreeMerger {
     private Node remote;
     
     public static boolean conflict;
+    private List<ImportDeclaration> imports;
     
     public TreeMerger(String baseFile, String localFile, String remoteFile) throws FileNotFoundException {
         this.base = JavaParser.parse(new File(baseFile));
         this.local = JavaParser.parse(new File(localFile));
         this.remote = JavaParser.parse(new File(remoteFile));
         
-        ((CompilationUnit) base).getImports().clear();
-        ((CompilationUnit) local).getImports().clear();
-        ((CompilationUnit) remote).getImports().clear();
+        this.imports = mergeImports((CompilationUnit) local, (CompilationUnit) remote);
+        
+        removeImports((CompilationUnit) base);
+        removeImports((CompilationUnit) local);
+        removeImports((CompilationUnit) remote);
     }
 
-    public Node merge() {
-        
+    public Node merge() {      
         base.accept(new NodeListWrapperVisitor(), "A"); 
         local.accept(new NodeListWrapperVisitor(), "B");
         remote.accept(new NodeListWrapperVisitor(), "C");
@@ -43,7 +51,8 @@ public class TreeMerger {
         
         conflict = false;
         
-        base.accept(new MergeVisitor(), new Pair<DiffResult, DiffResult>(localDiff, remoteDiff));   
+        Pair<DiffResult, DiffResult> diffs = new Pair<>(localDiff, remoteDiff);
+        base.accept(new MergeVisitor(), diffs);   
         
         if (conflict) {
             return null;
@@ -51,11 +60,39 @@ public class TreeMerger {
         
         base.accept(new NodeListUnwrapperVisitor(), null); 
         
+        addImports((CompilationUnit) base, imports);
+        
         return base;
     }
     
     public static void reportConflict() {
         conflict = true;
+    }
+    
+    private static List<ImportDeclaration> mergeImports(CompilationUnit local, CompilationUnit remote) {
+        List<ImportDeclaration> imports = new ArrayList<>(local.getImports());
+        for (ImportDeclaration imprt : remote.getImports()) {
+            if (!imports.contains(imprt)) {
+                imports.add(imprt);
+            }
+        }
+        Collections.sort(imports, new Comparator<ImportDeclaration>() {
+            @Override
+            public int compare(ImportDeclaration o1, ImportDeclaration o2) {
+                return o1.toString().compareTo(o2.toString());
+            }
+        });
+        return imports;
+    }
+    
+    private static void addImports(CompilationUnit cu, List<ImportDeclaration> imports) {
+        for (ImportDeclaration imprt : imports) {
+            ((CompilationUnit) cu).addImport(imprt);
+        } 
+    }
+    
+    private static void removeImports(CompilationUnit cu) {
+        cu.getImports().clear();
     }
 
 }
