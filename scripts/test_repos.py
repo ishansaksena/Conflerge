72c3718ae851651eeb355023eb209146fdfa6851
John Laws Harrison <johnlawsharrison@gmail.com>
2017-03-07 14:56:51 -0800

    Remove single repo check

--- scripts/test_repos.py
+++ scripts/test_repos.py
@@ -7,11 +7,10 @@ from github import Github
 g = Github("username", "password")
 
 for repo in g.search_repositories("language:java", sort="stars", order="desc"):
-	if repo.name == "weex":
-		# clone the repo to /tmp/
-		p = subprocess.Popen("git clone {0} && mv {1} /tmp/".format(repo.ssh_url, repo.name), shell=True).wait()
+	# clone the repo to /tmp/
+	p = subprocess.Popen("git clone {0} && mv {1} /tmp/".format(repo.ssh_url, repo.name), shell=True).wait()
 
-		# test the repo using Conflerge
-		p = subprocess.Popen("./test_repo.sh {0} {1}".format(repo.name, repo.full_name.replace('/', '-')), shell=True).wait()
-		# cleanup
-		p = subprocess.Popen("rm -rf /tmp/{0}".format(repo.name), shell=True).wait()
+	# test the repo using Conflerge
+	p = subprocess.Popen("./test_repo.sh {0} {1}".format(repo.name, repo.full_name.replace('/', '-')), shell=True).wait()
+	# cleanup
+	p = subprocess.Popen("rm -rf /tmp/{0}".format(repo.name), shell=True).wait()
